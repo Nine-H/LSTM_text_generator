da314d291d8c2e81f93e6faf9cfe382d06a12f9a
Nine-H <nine.gentooman@gmail.com>
2019-03-06 23:04:39 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Machine Learning Text Generator
+
+## Idea
+
+I wrote a scraper for comments from pornhub, I want to feed them into a machine-learning AI to generate newer and sadder comments.
+
+## Design
+
+* I'm stupid so I'm going to skate over details by using Keras.
+* Input will be a small dataset (1-50MB) of text catted together.
+* I'm going to steal as much code as possible to get a works on my machine implementation.
+* This is not gonna be super clean or pythonic or useful at all.
+
+## Notes
+
+
+
+## Reading List
+
+<http://karpathy.github.io/2015/05/21/rnn-effectiveness/>
+<https://towardsdatascience.com/recurrent-neural-networks-by-example-in-python-ffd204f99470>
+<https://deepsense.ai/keras-or-pytorch/>
+<https://www.analyticsvidhya.com/blog/2018/03/text-generation-using-python-nlp/>
+<https://minimaxir.com/2018/05/text-neural-networks/>
+<https://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/>
---
+++ regression.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Masking, Embedding
+from keras.utils import np_utils
+
+#
+# Load the Data
+#
+
+text =(open("input.txt").read())
+
+model = Sequential()
+
+# Embedding layer
+model.add(
+    Embedding(input_dim=num_words,
+              input_length = training_length,
+              output_dim=100,
+              weights=[embedding_matrix],
+              trainable=False,
+              mask_zero=True))
+
+# Masking layer for pre-trained embeddings
+model.add(Masking(mask_value=0.0))
+
+# Recurrent layer
+model.add(LSTM(64, return_sequences=False, 
+               dropout=0.1, recurrent_dropout=0.1))
+
+# Fully connected layer
+model.add(Dense(64, activation='relu'))
+
+# Dropout for regularization
+model.add(Dropout(0.5))
+
+# Output layer
+model.add(Dense(num_words, activation='softmax'))
+
+# Compile the model
+model.compile(
+    optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
